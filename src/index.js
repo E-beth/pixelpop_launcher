c6a9288b031ab3b4c6078d609ae7eb6eddbbabea
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-07-19 23:27:14 -0400

    add some window options

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const { app, BrowserWindow } = require('electron');
 function createWindow () {
   const win = new BrowserWindow({
     frame: false,
+    backgroundColor: '#000000',
+    fullscreen: true,
   });
 
   win.loadFile('./src/index.html');
