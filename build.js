962badee89e441e81653d25c9a7183429a4431f7
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-07-19 22:45:09 -0400

    add an icon

--- build.js
+++ build.js
@@ -1,5 +1,7 @@
 const fs = require('fs-extra');
 const path = require('path');
+const rcedit = require('rcedit');
+const package = require('./package.json');
 
 const outputDir = './build';
 
@@ -9,3 +11,9 @@ fs.copySync('./node_modules/electron/dist', outputDir);
 fs.emptyDirSync(path.join(outputDir, 'resources/app'));
 fs.copySync('./src', path.join(outputDir, 'resources/app'));
 fs.copySync('./package.json', path.join(outputDir, 'resources/app/package.json'));
+rcedit(path.join(outputDir, 'electron.exe'), {
+	'version-string': package.version,
+	'file-version': package.version,
+	'product-version': package.version,
+	icon: './icon.ico',
+});
